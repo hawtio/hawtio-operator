d05895b8eb4b5bc2bc90c3db51e581cc22bc0838
Antonin Stefanutti <antonin@stefanutti.fr>
2020-09-08 15:49:31 +0200

    feat: Generate OpenAPI structural schema for Hawtio CRD

--- pkg/apis/hawtio/v1alpha1/hawtio_types.go
+++ pkg/apis/hawtio/v1alpha1/hawtio_types.go
@@ -7,8 +7,10 @@ import (
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
 
 // HawtioDeploymentType defines the possible deployment types
+// +kubebuilder:validation:Enum=cluster;namespace
 type HawtioDeploymentType string
 
 const (
@@ -22,39 +24,76 @@ const (
 	NamespaceHawtioDeploymentType HawtioDeploymentType = "namespace"
 )
 
-// HawtioSpec defines the desired state of Hawtio
+// +k8s:openapi-gen=true
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+// +kubebuilder:resource:path=hawtios,scope=Namespaced,categories=hawtio
+// +kubebuilder:subresource:status
+// +kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas,selectorpath=.status.selector
+// +kubebuilder:printcolumn:name="Age",type=date,JSONPath=`.metadata.creationTimestamp`,description="Creation phase"
+// +kubebuilder:printcolumn:name="Image",type=string,JSONPath=`.status.image`,description="Console image"
+// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`,description="Console phase"
+// +kubebuilder:printcolumn:name="URL",type=string,JSONPath=`.status.URL`,description="Console URL"
+
+// Hawtio Console
+type Hawtio struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   HawtioSpec   `json:"spec,omitempty"`
+	Status HawtioStatus `json:"status,omitempty"`
+}
+
+// Defines the desired state of Hawtio
 type HawtioSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	Type          HawtioDeploymentType        `json:"type,omitempty"`
-	Replicas      *int32                      `json:"replicas,omitempty"`
-	RouteHostName string                      `json:"routeHostName,omitempty"`
-	Version       string                      `json:"version,omitempty"`
-	RBAC          HawtioRBAC                  `json:"rbac,omitempty"`
-	Resources     corev1.ResourceRequirements `json:"resources,omitempty"`
+	// The deployment type. Defaults to cluster.
+	// cluster: Hawtio is capable of discovering and managing
+	// applications across all namespaces the authenticated user
+	// has access to.
+	// namespace: Hawtio is capable of discovering and managing
+	// applications within the deployment namespace.
+	Type HawtioDeploymentType `json:"type,omitempty"`
+	// Number of desired pods. This is a pointer to distinguish between explicit
+	// zero and not specified. Defaults to 1.
+	Replicas *int32 `json:"replicas,omitempty"`
+	// The edge host name of the route that exposes the Hawtio service
+	// externally. If not specified, it is automatically generated and
+	// is of the form:
+	// <name>[-<namespace>].<suffix>
+	// where <suffix> is the default routing sub-domain as configured for
+	// the cluster.
+	// Note that the operator will recreate the route if the field is emptied,
+	// so that the host is re-generated.
+	RouteHostName string `json:"routeHostName,omitempty"`
+	// The Hawtio console container image version. Defaults to 'latest'.
+	Version string     `json:"version,omitempty"`
+	RBAC    HawtioRBAC `json:"rbac,omitempty"`
+	// The compute resources
+	Resources corev1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 // The RBAC configuration
 type HawtioRBAC struct {
-	// Whether RBAC is enabled.
-	// Default is true.
+	// Whether RBAC is enabled. Defaults to true.
 	Enabled *bool `json:"enabled,omitempty"`
 	// The name of the ConfigMap that contains the ACL definition.
 	ConfigMap string `json:"configMap,omitempty"`
 }
 
-// HawtioStatus defines the observed state of Hawtio
+// Reports the observed state of Hawtio
 type HawtioStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
-	Image    string      `json:"image,omitempty"`
-	Phase    HawtioPhase `json:"phase,omitempty"`
-	URL      string      `json:"URL,omitempty"`
-	Replicas int32       `json:"replicas,omitempty"`
-	Selector string      `json:"selector,omitempty"`
+	// The Hawtio console container image
+	Image string      `json:"image,omitempty"`
+	Phase HawtioPhase `json:"phase,omitempty"`
+	// The Hawtio console route URL
+	URL string `json:"URL,omitempty"`
+	// The actual number of pods
+	Replicas int32 `json:"replicas,omitempty"`
+	// The label selector for the Hawtio pods
+	Selector string `json:"selector,omitempty"`
 }
 
-// HawtioPhase --
+// The Hawtio deployment phase
+// +kubebuilder:validation:Enum=Initialized;Deployed;Failed
 type HawtioPhase string
 
 const (
@@ -65,18 +104,6 @@ const (
 	// HawtioPhaseFailed --
 	HawtioPhaseFailed HawtioPhase = "Failed"
 )
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// Hawtio is the Schema for the hawtios API
-// +k8s:openapi-gen=true
-type Hawtio struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   HawtioSpec   `json:"spec,omitempty"`
-	Status HawtioStatus `json:"status,omitempty"`
-}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
