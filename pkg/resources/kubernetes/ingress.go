b75b8ce9eaea521f3abe1a7fcdb4cf5cd3d32019
phantomjinx <p.g.richardson@phantomjinx.co.uk>
2024-01-12 19:11:25 +0000

    feat: Adds notional support for extracting the url into the hawtio.Status #100
    * While the extraction of the route url is quite straightforward, the
      use of ingress path rules and no hosts makes the return of a single
      string quite difficult. This may well need work in the future to
      improve the value returned

--- pkg/resources/kubernetes/ingress.go
+++ pkg/resources/kubernetes/ingress.go
@@ -1,6 +1,8 @@
 package kubernetes
 
 import (
+	"strconv"
+
 	hawtiov1 "github.com/hawtio/hawtio-operator/pkg/apis/hawtio/v1"
 	corev1 "k8s.io/api/core/v1"
 	networkingv1 "k8s.io/api/networking/v1"
@@ -66,4 +68,73 @@ func NewIngress(hawtio *hawtiov1.Hawtio, servingSecret *corev1.Secret) *networki
 	}
 
 	return ingress
+}
+
+// GetIngressURL determines the full URL of the given ingress
+func GetIngressURL(ingress *networkingv1.Ingress) string {
+	var scheme string
+	if len(ingress.Spec.TLS) > 0 {
+		scheme = "https"
+	} else {
+		scheme = "http"
+	}
+
+	host, port := getIngressHostAndPort(ingress)
+	path := getIngressPath(ingress)
+
+	url := scheme + "://" + host
+	if len(port) > 0 {
+		url = url + ":" + port
+	}
+
+	return url + path
+}
+
+func getIngressHostAndPort(ingress *networkingv1.Ingress) (string, string) {
+	ingressStatuses := ingress.Status.LoadBalancer.Ingress
+	if len(ingressStatuses) == 0 {
+		for _, ingressRule := range ingress.Spec.Rules {
+			if len(ingressRule.Host) > 0 {
+				return ingressRule.Host, ""
+			}
+		}
+
+		return "*", "" // host must be a wildcard
+	}
+
+	// get host or ip of the first ingress status available
+	var host string
+	port := ""
+	for _, ingressStatus := range ingress.Status.LoadBalancer.Ingress {
+		if len(ingressStatus.Hostname) > 0 {
+			host = ingressStatus.Hostname
+		} else if len(ingressStatus.IP) > 0 {
+			host = ingressStatus.IP
+		}
+
+		if len(host) > 0 {
+			for _, statusPort := range ingressStatus.Ports {
+				port = strconv.FormatInt(int64(statusPort.Port), 10)
+				continue // get the first port
+			}
+		}
+	}
+
+	if len(host) == 0 {
+		return "*", port
+	}
+
+	return host, port
+}
+
+func getIngressPath(ingress *networkingv1.Ingress) string {
+	for _, ingressRule := range ingress.Spec.Rules {
+		if ingressRule.IngressRuleValue.HTTP != nil {
+			for _, httpPath := range ingressRule.IngressRuleValue.HTTP.Paths {
+				return httpPath.Path
+			}
+		}
+	}
+
+	return "/"
 }
